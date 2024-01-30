4321f4d3eea670c80a3435db17e317e428c8b9d7
cjordan239 <chris.jordanig@gmail.com>
2024-01-30 10:52:04 +0700

    adding match snapshot

--- my-app/src/Component/Footer/Footer.test.tsx
+++ my-app/src/Component/Footer/Footer.test.tsx
@@ -2,16 +2,19 @@ import { render, screen } from '@testing-library/react';
 import Footer from '.';
 
 describe('Unit test for Footer', () => {
-
     test('should render footer with social media icons', () => {
         render(<Footer />);
         expect(screen.getByAltText('Facebook')).toBeDefined();
         expect(screen.getByAltText('Twitter')).toBeDefined();
         expect(screen.getByAltText('Instagram')).toBeDefined();
+        expect(screen.getByAltText('Facebook')).toMatchSnapshot();
+        expect(screen.getByAltText('Twitter')).toMatchSnapshot();
+        expect(screen.getByAltText('Instagram')).toMatchSnapshot();
     });
 
     test('should render footer with "Buy me a coffee" link', () => {
         render(<Footer />);
         expect(screen.getByText('Buy me a coffee')).toBeDefined();
+        expect(screen.getByText('Buy me a coffee')).toMatchSnapshot();
     });
 });
--- my-app/src/Component/NavBar/Navbar.test.tsx
+++ my-app/src/Component/NavBar/Navbar.test.tsx
@@ -1,19 +1,17 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import NavBar from '.';
 import React from 'react';
 
-
-describe ('Unit test for NavBar', () => {
-
+describe('Unit test for NavBar', () => {
     it('should render the navbar with login', () => {
-        render(<NavBar />)
-        
+        render(<NavBar />);
         expect(screen.getByText('Login')).toBeDefined();
-    })
+        expect(screen.getByText('Login')).toMatchSnapshot();
+    });
 
     test('should render navbar with home ', () => {
-        render(<NavBar />)
-
+        render(<NavBar />);
         expect(screen.getByText('Login')).toBeDefined();
-    })
-})
+        expect(screen.getByText('Login')).toMatchSnapshot();
+    });
+});
